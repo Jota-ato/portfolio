45c9e65edea2f60d88aa7c04bdb594fe4e642719
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-07-06 18:05:38 -0600

    feat(hero.tsx): add grid display: -adjust to have a min heigh of 30 rem -add profile image

--- src/shared/components/landing/hero.tsx
+++ src/shared/components/landing/hero.tsx
@@ -1,18 +1,32 @@
 import { Heading } from "@/shared/typography/heading";
+import Image from "next/image";
 
 export function Hero() {
     return (
-        <main className="flex flex-col gap-4 my-4 md:my-8">
-            <Heading>
-                Julio César Zavala Blanco
-            </Heading>
-            <span className="text-lg font-medium text-primary">
-                Custom Software & Automation for SMBs
-            </span>
-            <p className="max-w-md text-foreground/80">
-                I build high-performance web systems, internal dashboards, and custom automations that help local businesses save time and protect their margins.
-                <span className="text-foreground"> Zero generic templates. Just tailored software engineered to optimize your operations.</span>
-            </p>
+        <main className="grid grid-cols-1 md:grid-cols-2 items-center gap-4 my-4 md:my-8 min-h-120">
+            <div className="space-y-4">
+                <Heading>
+                    Julio César Zavala Blanco
+                </Heading>
+                <p className="text-lg font-bold text-primary">
+                    Custom Software & Automation for SMBs
+                </p>
+                <p className="max-w-sm text-foreground/80">
+                    I build high-performance web systems, internal dashboards, and custom automations that help local businesses save time and protect their margins.
+                    <span className="text-foreground"> Zero generic templates. Just tailored software engineered to optimize your operations.</span>
+                </p>
+            </div>
+            <div>
+                <Image 
+                    src={"/img/me.jpeg"}
+                    alt="Image of Julio César Zavala Blanco"
+                    width={400}
+                    height={400}
+                    loading="eager"
+                    priority
+                    className="aspect-square rounded-full"
+                />
+            </div>
         </main>
     )
 }
