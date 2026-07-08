1e70a4526229fe8e76d3ece8ee28713deda9d0ab
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-07-08 17:59:12 -0600

    s

--- src/features/landing/components/landing/projects.tsx
+++ src/features/landing/components/landing/projects.tsx
@@ -1,11 +1,13 @@
 import { Heading } from "@/shared/typography/heading";
 import { Container } from "../../../../shared/components/ui/container";
 
+
 export function ProjectsSection() {
     return (
         <section className="py-8 md:py-12 min-h-screen bg-secondary text-secondary-foreground">
-            <Container>
+            <Container className="space-y-8">
                 <Heading level={2}>My work</Heading>
+                
             </Container>
         </section>
     )
